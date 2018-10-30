e737873ae370d76014811644735e5eeddaad81aa
Kagu-chan <k.boese@kagu-chan.de>
2018-10-30 23:58:15 +0100

    Start writing the beatmap analyzer

--- lib/osu/tool/analyzer/analyzer.rb
+++ lib/osu/tool/analyzer/analyzer.rb
@@ -5,15 +5,55 @@ module Osu
       def self.run(args)
         if (args == ['-help']) then
           self.help()
+        else
+          self.new(*args).run()
+        end
+      end
+
+      def initialize(mapset, *args)
+        @mapset_location = mapset
+        @maps = []
+
+        loop {
+          arg = args[0]
+          if arg && arg.start_with?('-') && arg != '-all' then
+            option, value = args.shift().sub(/^-/, '').split('=')
+
+            instance_variable_set("@#{option}", value || true)
+          else
+            break
+          end
+        }
+
+        if args == ['-all'] then
+          @maps = []
+        else
+          @maps = args
+        end
+      end
+
+      def run()
+        @mapset = Osu::MapSet::MapSet.new(@mapset_location)
+        @mapset.load()
+
+        @mapset.open_storyboard() if !@skipstoryboard
+
+        if @maps.size == 0 then
+          @mapset.load_all_beatmaps()
+        else
+          @maps.each() { |map| @mapset.load_beatmap(map) }
         end
       end
 
 private
       def self.help()
         puts <<TXT
-Usage: ruby osr.rb analyzer MAP [OPTIONS] [DIFFS|-all]
+Usage: ruby osr.rb analyzer MAP [OPTIONS] DIFFS|-all
   MAP        => beatmap directory
-  OPTIONS    => none so far
+
+  OPTIONS    =>
+                -skipstoryboard => If true, no attempt is made to load a storyboard
+
   DIFFS|-all => list of difficulties to respect.
                 type `-all` to load all difficulties
 
