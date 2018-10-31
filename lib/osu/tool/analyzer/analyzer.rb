ac0259b12e2856a20a9080d28bf66645e59e6c90
Kagu-chan <k.boese@kagu-chan.de>
2018-10-31 03:13:05 +0100

    Change arguments for analyzer

--- lib/osu/tool/analyzer/analyzer.rb
+++ lib/osu/tool/analyzer/analyzer.rb
@@ -11,12 +11,24 @@ module Osu
       end
 
       def initialize(mapset, *args)
+        ##
+        # option skipstoryboard (boolean)
+        #
+        # if true, no storyboard will be loaded
+        @skipstoryboard = false
+
+        ##
+        # option all (boolean)
+        #
+        # If true, beatmap argument will be ignored and all maps will be loaded
+        @all = false
+
         @mapset_location = mapset
         @maps = []
 
         loop {
           arg = args[0]
-          if arg && arg.start_with?('-') && arg != '-all' then
+          if arg && arg.start_with?('-') then
             option, value = args.shift().sub(/^-/, '').split('=')
 
             instance_variable_set("@#{option}", value || true)
@@ -25,11 +37,7 @@ module Osu
           end
         }
 
-        if args == ['-all'] then
-          @maps = []
-        else
-          @maps = args
-        end
+        @maps = @all ? [] : args
       end
 
       def run()
@@ -48,14 +56,14 @@ module Osu
 private
       def self.help()
         puts <<TXT
-Usage: ruby osr.rb analyzer MAP [OPTIONS] DIFFS|-all
+Usage: ruby osr.rb analyzer MAP [OPTIONS] [DIFFS[]]
   MAP        => beatmap directory
 
-  OPTIONS    =>
-                -skipstoryboard => If true, no attempt is made to load a storyboard
+  OPTIONS   =>
+              -skipstoryboard => If true, no attempt is made to load a storyboard
+              -all            => If true, DIFFS argument will be ignored and all maps will be loaded
 
-  DIFFS|-all => list of difficulties to respect.
-                type `-all` to load all difficulties
+  DIFFS[]   => list of difficulties to respect
 
 TXT
       end
